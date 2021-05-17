9ed5d094cec65ca53e9544c40b0e607e7ba16b57
Kim Örnberg <kim.ornberg@skola.botkyrka.se>
2021-05-17 14:46:40 +0200

    test

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+window.document.body.classList.toggle('dark')
+import { fade } from 'svelte/transition';
+
+</script>
+
+<main>
+	
+<h1 transition:fade>Crypto</h1>
+
+<input type="text" placeholder="Password" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"><br><br>
+<button>Login</button>
+</main>
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 12em;
+		max-width: 240px;
+		margin: 0 auto;
+		
+	}
+	
+	:global(body.dark) {
+		background-color: #1d3040;
+	}
+
+	button {
+		background-color: #f76027;
+		color: white;
+		border: none;
+		border-radius: 50px;
+		padding: 0.75rem;
+		text-transform: uppercase;
+		
+	}
+	h1 {
+		color: #ffffff;
+		text-transform: uppercase;
+		font-size: 7em;
+		font-weight: 500;
+		background-color: #1d3040;
+	
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+	
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+});
+
+export default app;
+
+/*
+let password = "qwerty"
+
+if (password = "qwerty") {
+	return true
+} else {
+	return false
+}
+*/
